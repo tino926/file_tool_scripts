0428cb13e01270a891227330f76c16b067c77c4c
tino926 <tino926@users.noreply.github.com>
2024-03-15 21:56:33 +0800

    make variable name more meaningful

--- onedrive_tool/list_onedrive_folders_2.py
+++ onedrive_tool/list_onedrive_folders_2.py
@@ -4,8 +4,8 @@ from requests_oauthlib import OAuth2Session
 import os
 
 # Replace these with your actual client ID and secret
-client_id = 'YOUR_CLIENT_ID'
-client_secret = 'YOUR_CLIENT_SECRET'
+app_client_id = 'YOUR_CLIENT_ID'
+app_client_secret = 'YOUR_CLIENT_SECRET'
 
 # Check if the ./pri directory exists and contains settings.ini
 settings_path = './pri/settings.ini'
@@ -13,13 +13,13 @@ if os.path.exists(settings_path):
     # Use configparser to read the settings
     config = configparser.ConfigParser()
     config.read(settings_path)
-    client_id = config.get('DEFAULT', 'client_id')
-    client_secret = config.get('DEFAULT', 'client_secret')
-    tenant_id = config.get('DEFAULT', 'tenant_id')
+    app_client_id = config.get('DEFAULT', 'client_id')
+    app_client_secret = config.get('DEFAULT', 'client_secret')
+    drive_tenant_id = config.get('DEFAULT', 'tenant_id')
 else:
     # Fallback to hardcoded values or prompt the user
-    client_id = 'YOUR_CLIENT_ID'
-    client_secret = 'YOUR_CLIENT_SECRET'
+    app_client_id = 'YOUR_CLIENT_ID'
+    app_client_secret = 'YOUR_CLIENT_SECRET'
 
 
 # Set the necessary scopes for accessing OneDrive
@@ -28,7 +28,7 @@ scopes = ['https://graph.microsoft.com/Files.ReadWrite.All']
 # Create an OAuth2Session instance
 auth_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
 token_url = 'https://login.microsoftonline.com/common/oauth2/v2.0/token'
-oauth = OAuth2Session(client_id, scope=scopes, redirect_uri='http://localhost')
+oauth = OAuth2Session(app_client_id, scope=scopes, redirect_uri='http://localhost')
 
 # Fetch the authorization URL and prompt the user to grant permissions
 authorization_url, state = oauth.authorization_url(auth_url)
@@ -38,7 +38,7 @@ print('Please go to this URL and grant permissions: {}'.format(authorization_url
 redirect_response = input('Enter the full redirect URL: ')
 
 # Fetch the access token
-token = oauth.fetch_token(token_url, client_secret=client_secret, authorization_response=redirect_response)
+token = oauth.fetch_token(token_url, client_secret=app_client_secret, authorization_response=redirect_response)
 
 # Set up the headers for the Microsoft Graph API requests
 headers = {'Authorization': 'Bearer {}'.format(token['access_token'])}
