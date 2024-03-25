47c63308eeadb85b00cb72f6c0b105cf3e338498
tino926 <tino926@users.noreply.github.com>
2024-03-25 21:55:29 +0800

    functionalize extract_frames_1fps.py

--- video_proc/extract_frames_1fps.py
+++ video_proc/extract_frames_1fps.py
@@ -13,22 +13,34 @@ else:
     video_dir = os.getcwd()
 
 
-# List of video file extensions to process
-video_extensions = ['.mp4', '.avi', '.mkv']
 
-# Iterate over all files in the current directory
-for filename in os.listdir(video_dir):
-    # Check if the file has one of the video extensions
-    if any(filename.endswith(ext) for ext in video_extensions):
-        # Extract the base name without the extension
-        base_name = os.path.splitext(filename)[0]
-        # Create a new directory with the base name
-        new_dir = os.path.join(video_dir, base_name)
-        os.makedirs(new_dir, exist_ok=True)
+def extract_frames(video_dir):
 
-        # Construct the ffmpeg command
-        stream = ffmpeg.input(os.path.join(video_dir, filename))
-        stream = ffmpeg.output(stream, new_dir+'/%08d.jpg', q=1, 
-                               fps_mode='passthrough', 
-                               frame_pts=1)
-        ffmpeg.run(stream)
+    # List of video file extensions to process
+    video_extensions = ['.mp4', '.avi', '.mkv']
+
+    # Iterate over all files in the current directory
+    for filename in os.listdir(video_dir):
+        # Check if the file has one of the video extensions
+        if any(filename.endswith(ext) for ext in video_extensions):
+            # Extract the base name without the extension
+            base_name = os.path.splitext(filename)[0]
+            # Create a new directory with the base name
+            new_dir = os.path.join(video_dir, base_name)
+            os.makedirs(new_dir, exist_ok=True)
+
+            # Construct the ffmpeg command
+            stream = ffmpeg.input(os.path.join(video_dir, filename))
+            stream = ffmpeg.output(stream, new_dir+'/%08d.jpg', q=1, 
+                                fps_mode='passthrough', 
+                                frame_pts=1)
+            try:
+                ffmpeg.run(stream)
+            except ffmpeg.Error as e:
+                print(e)
+
+def main():
+    extract_frames(video_dir=video_dir)
+
+if __name__ == "__main__":
+    main()
